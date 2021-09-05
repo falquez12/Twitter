b7a5f65fb7e6443058be93bf18e6028dcb6610fe
Sebastian <sesantander@uninorte.edu.co>
2021-09-05 18:19:02 -0500

    feat(Trending)desc: added Trending component,Trending elements component,Css for those components and dummies.js

---
+++ src/components/Private/Timeline/Trending/elements.jsx
@@ -0,0 +1,20 @@
+import {
+ ElementContainer,
+ ElementH1,
+ ElementWord,
+ ElementLine
+} from "./trendingelements";
+const Elements = (props) => {
+    const {trending, hashtag, ntweets} = props;
+    return (
+        <>  
+        <ElementContainer>
+            <ElementWord>{trending}</ElementWord>
+            <ElementH1>{hashtag}</ElementH1>
+            <ElementWord>{ntweets}</ElementWord>
+            </ElementContainer>
+        </>
+    );
+}
+
+export default Elements;
