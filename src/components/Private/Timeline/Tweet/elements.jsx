2665f3f34bfcde911d0376c60ea519bdbb1eead6
Sebastian <sesantander@uninorte.edu.co>
2021-09-05 22:33:01 -0500

    Tweet Component added

--- src/components/Private/Timeline/Tweet/elements.jsx
+++ src/components/Private/Timeline/Tweet/elements.jsx
@@ -1,16 +1,49 @@
 import {
  TweetContainer,
- TrendingTitle,
- Showmore
+ TweetName,
+ TweetText,
+ TweetImage,
+ TweetIcons,
+ Container,
+ ProfilePicture
 } from "./tweetelements";
+import { GoVerified } from 'react-icons/go';
+import { BiMessageRounded } from 'react-icons/bi';
+import { FaRetweet } from 'react-icons/fa';
+import { AiOutlineHeart } from 'react-icons/ai';
+import { HiInboxIn } from 'react-icons/hi';
+
+import tweet1 from "../../../../imgs/tweet1.jpg"
+import profilepicture1 from "../../../../imgs/profilepicture1.jpg"
 
 const TweetElement = () => {
     return (
-        <>
-            <TweetContainer>
-            <TrendingTitle>Paris Trend</TrendingTitle>
-            <Showmore>Show more</Showmore>
-            </TweetContainer>
+        <>  
+            <Container>
+            <ProfilePicture src={profilepicture1} alt="profilepicture" height="40" width="40"/>
+                <TweetContainer>
+                
+                <TweetName>
+                    <p>Annie</p>
+                    <p><GoVerified/></p>
+                    <p>@annie</p>
+                    <p>.</p>
+                    <p>14s</p>
+                </TweetName>
+                <TweetText>
+                    This is a tweet. It can be long, or short. Depends on what you have to say. It can have some hashtags too. #likethis This is a tweet. It can be long, or short. Depends on what you have to say. It can have some hashtags too. #likethis 
+                </TweetText>
+                
+                <TweetImage><img src={tweet1} alt="profile" height="285" width="574"/></TweetImage>
+                
+                <TweetIcons>
+                    <div><BiMessageRounded></BiMessageRounded></div>
+                    <p><FaRetweet></FaRetweet></p>
+                    <p><AiOutlineHeart></AiOutlineHeart></p>
+                    <p><HiInboxIn></HiInboxIn></p>
+                </TweetIcons>
+                </TweetContainer>
+            </Container>
         </>
     );
 }
--- src/components/Private/Timeline/Tweet/tweetelements.jsx
+++ src/components/Private/Timeline/Tweet/tweetelements.jsx
@@ -2,18 +2,73 @@ import styled from "styled-components";
 import { Row, Col } from "react-bootstrap";
 import { H1 ,P, A } from "../../../../lib/ui/text";
 
-export const Tweetelements = styled(Col)`
-  
-    align-items: center;
-    padding: 14px 0px;
-    position: static;
-    width: 280px;
-    height: 645px;
-    left: 0px;
-    top: 64px;
-    background: #F6F8FA;
-    border-radius: 12px;
+export const TweetContainer = styled(Col)`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 0px;
+    background: #FFFFFF;
 
+    position: static;
+    width: 692px;
+    height: 432px;
+    left: 0px;
+    top: 0px;
 `;
 
 
+export const TweetName = styled.div`
+    display:flex;
+    flex-direction:row;
+    & > p { 
+        margin-left:5px;
+    }
+`;
+export const TweetText = styled.p`
+    width: 574px;
+    height: 66px;
+    left: 56px;
+    top: 30px;
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 15px;
+    line-height: 22px;
+    color: #333333;
+`;
+
+
+export const TweetImage = styled.p`
+    border-radius: 12px;
+    & > span {
+        .icon-verified:before {
+        content: "\e900";
+        }
+    }
+`;
+
+
+export const TweetIcons = styled.div`
+    display:flex;
+    flex-direction:row;
+    & > p {
+        color:#828282;
+         margin-left:134px;
+    }
+    & > div { 
+        margin-left:25px;
+        color:#828282;
+    }
+`;
+
+export const Container = styled.div`
+    display:flex;
+    flex-direction:row;
+    & > p { 
+        margin-left:5px;
+    }
+`;
+export const ProfilePicture = styled.img`
+        border-radius: 52%;
+
+`;
