6fa8a0c20d2c29e9c240b1559e6c12c2cab1678b
JesusRomeroB <romerojesus@uninorte.edu.co>
2021-09-06 09:44:12 -0500

    Searchbar

--- src/components/Private/Timeline/index.jsx
+++ src/components/Private/Timeline/index.jsx
@@ -1,13 +1,9 @@
 import Trending from "./Trending/index";
 import Tweet from "./Tweet/index";
 import Navbar from "../../common/Nav/index";
-<<<<<<< HEAD
 import { ColNav } from "./timelineelements";
 import { Row, Col, Container } from "react-bootstrap";
-=======
 import Searchbar from "../Search/searchbar.jsx";
->>>>>>> f69d420458f527bdd37c91fbb9f7d2aa0c97cec1
-
 const Timeline = () => {
   return (
     <>
@@ -22,20 +18,13 @@ const Timeline = () => {
             <Tweet></Tweet>
           </Col>
           <Col xs={3}>
+            <Searchbar/>
             <Trending></Trending>
-<<<<<<< HEAD
           </Col>
         </Row>
       </Container>
     </>
   );
 };
-=======
-            <Searchbar></Searchbar>
-            </Container>
-        </>
-    );
-}
->>>>>>> f69d420458f527bdd37c91fbb9f7d2aa0c97cec1
 
 export default Timeline;
