f856f07dd5e3873f773bbebb0659a54c46a788a7
JesusRomeroB <romerojesus@uninorte.edu.co>
2021-09-06 09:59:12 -0500

    searchbar fix

--- src/components/Private/Timeline/index.jsx
+++ src/components/Private/Timeline/index.jsx
@@ -1,12 +1,18 @@
 import Trending from "./Trending/index";
 import Tweet from "./Tweet/index";
+import Profile from "./Profile";
+import Notifications from "./Notifications";
+import Lists from "./Lists";
+import Messages from "./Messages";
+import Bookmarks from "./Bookmarks";
 import Navbar from "../../common/Nav/index";
 import { ColNav } from "./timelineelements";
 import { Row, Col, Container } from "react-bootstrap";
 import Searchbar from "../Search/searchbar.jsx";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 const Timeline = () => {
   return (
-    <>
+    <BrowserRouter>
       <Container>
         <Row>
           <ColNav xs={3}>
@@ -15,15 +21,34 @@ const Timeline = () => {
             </div>
           </ColNav>
           <Col xs={6}>
-            <Tweet></Tweet>
+            <Switch>
+              <Route path="/home">
+                <Tweet></Tweet>
+              </Route>
+              <Route path="/profile">
+                <Profile></Profile>
+              </Route>
+              <Route path="/lists">
+                <Lists></Lists>
+              </Route>
+              <Route path="/bookmarks">
+                <Bookmarks></Bookmarks>
+              </Route>
+              <Route path="/messages">
+                <Messages></Messages>
+              </Route>
+              <Route path="/notifications">
+                <Notifications></Notifications>
+              </Route>
+            </Switch>
           </Col>
           <Col xs={3}>
-            <Searchbar/>
+            <Searchbar />
             <Trending></Trending>
           </Col>
         </Row>
       </Container>
-    </>
+    </BrowserRouter>
   );
 };
 
