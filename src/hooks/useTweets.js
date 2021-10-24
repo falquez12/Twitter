2bb3a55d5ddabf5a79b2ef1472dd866df0a0d1b1
Sebastian <sesantander@uninorte.edu.co>
2021-10-24 13:52:42 -0500

    feat: notification handler added

--- src/hooks/useTweets.js
+++ src/hooks/useTweets.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Swal from 'sweetalert2'
 import {getTweets, createTweets, deleteTweets, likeTweets } from "./../services/tweetService";
 
 export const useTweets = () => {
@@ -15,12 +16,40 @@ export const useTweets = () => {
     }, []);
 
     const addTweet = async (content) => {
-        const result = await createTweets(content)
+        const result = await createTweets(content);
+        if(result){
+            Swal.fire({
+                title: 'Success!',
+                text: 'Tweet created successfully',
+                icon: 'success',
+                confirmButtonText: 'Cool'
+            })
+        }else{
+            Swal.fire({
+                title: 'Error!',
+                text: 'Something went wrong!',
+                icon: 'error',
+            })
+        }
         listTweets();
     };
 
     const removeTweet = async (id) => {
         const result = await deleteTweets(id)
+        if(result){
+            Swal.fire({
+                title: 'Success!',
+                text: 'Tweet deleted',
+                icon: 'success',
+                confirmButtonText: 'Cool'
+            })
+        }else{
+            Swal.fire({
+                title: 'Error!',
+                text: 'Something went wrong!',
+                icon: 'error',
+            })
+        }
         listTweets();
     };
 
